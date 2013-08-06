b9d1cf620d604c870055e77c980e4331cb843c05
BretHribar <cutcoedgecomeinterslice@gmail.com>
2013-08-06 19:39:32 +0000

    Edited index.html for branch test

--- grader.js
+++ grader.js
@@ -24,8 +24,13 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = fs.writeFile("tempURLfile.html",rest.get("http://aqueous-escarpment-3217.herokuapp.com/"));
+var testURLfile = fs.writeFile("tempURLfile.html",rest.get("http://aqueous-escarpment-3217.herokuapp.com/"));
+
+//var printToConsoleTest = function(console.log(testURLfile);
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,6 +39,35 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var assertURLExists = function(inURL,checksfile) {
+    console.log("begin assertURL fn on " + inURL);
+    rest.get(inURL).on('complete', function(result) {
+        if (result instanceof Error) {
+            console.log("Error retrieving URL");
+            console.log(Error);
+            process.exit(1);
+        } else {
+/*            $ = cheerio.load(result);
+            var checks = loadChecks(checksfile).sort();
+            var out = {};
+            for (var ii in checks) {
+                var presents = $(checks[ii]).length > 0;
+                out[checks[ii]] = present;
+            }
+            var outJson = JSON.stringify(out,null,4);
+            console.log(outJson);
+            console.log("did we fetch a URL?"); */
+	    var r=new Buffer( result);
+//console.log(result);
+fs.writeFile('urltext.html',r.toString('utf-8'));
+	    var checkJson = checkHtmlFile(r.toString('utf-8'),CHECKSFILE_DEFAULT);
+	    var outJson = JSON.stringify(checkJson, null, 4);
+	    console.log(outJson);
+        }
+      });
+    console.log("exiting assertURL fn");
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -65,10 +99,20 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_file>', 'URL address to html file', clone(assertURLExists), URL_DEFAULT)
         .parse(process.argv);
+    if (program.url) {
+	console.log('in url');
+	//assertURLExists(myArgs[3],program.checks);
+//var checkJson = checkFile(program.file, program.checks);
+
+    }
+    else {
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
--- web.js
+++ web.js
@@ -3,17 +3,17 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var fileIn = fs.readFileSync('index.html');
-
-//var bufferOut = fileIn. 
+//var fileIn = fs.readFileSync('index.html');
+ 
 
 app.get('/', function(request, response) {
 
+  var fileIn = fs.readFileSync('index.html');
   response.send(fileIn.toString());
 
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
